9307a2af7b25b8944c864bfa63765c0da7eb185d
GuiCoelho <guiminervino@gmail.com>
2021-09-29 20:08:05 -0300

    TA PRONTO ESSA BAGAÇA

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,17 @@
-var sessionTimeSubmit, breakTimeSubmit
-const form = document.getElementById("form")
+let sessionTimeSubmit;
 
-localStorage.setItem("sessionTime", 25)
-localStorage.setItem("breakTime", 5)
+const form = document.getElementById("form");
+
+localStorage.setItem("sessionTime", 25);
+localStorage.setItem("breakTime", 5);
 
 const sessionTime = document.getElementById("sessionTime");
+sessionTime.addEventListener("change", function (evento) {
+  sessionTimeSubmit = evento.target.value;
+  localStorage.setItem("sessionTime", sessionTimeSubmit);
+});
 
-sessionTime.addEventListener("change", function (e) {
-  sessionTimeSubmit = e.target.value;
-  localStorage.setItem("sessionTime", sessionTimeSubmit)
-  // TODO: arrumar pra armazenar os dados no localStorage no submit do form
-})
-
-const breakTime = document.getElementById("breakTime");
-breakTime.addEventListener("change", function (e) {
-  breakTimeSubmit = e.target.value;
-  localStorage.setItem("breakTime", breakTimeSubmit)
-  //TODO: arrumar pra armazenar os dados no localStorage no submit do form
-})
-
-form.addEventListener("submit", function (e) {
-  form.action = 'pomodoro.html';
-})
-
+form.addEventListener("submit", function () {
+  form.action = "pomodoro.html";
+});
+  
--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -1,73 +1,56 @@
 let minutes = localStorage.getItem("sessionTime");
 let seconds = "00";
 
-/* var sound = new Audio("../assets/ragnarok-online-level-up-sound.mp3"); */
-function github() {
-  window.open("https://www.github.com/guilhermea23", "_blank");
-}
-function template() {
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
-}
-
-
+document.getElementById("minutes").innerHTML = minutes
+  .toString()
+  .padStart(2, "0");
+document.getElementById("seconds").innerHTML = seconds;
 
 let buttonStart = document.getElementById("start");
-let buttonPause = document.getElementById("stop");
+let buttonStop = document.getElementById("stop");
 
-var minutes_storage = minutes - 1;
-var seconds_storage = 59;
-
-// console.log(buttonStart)
-
+let minutes_storage = minutes - 1;
+let seconds_storage = 59;
 
 buttonStart.addEventListener("click", function () {
-  console.log("Apertou o play");
   buttonStart.hidden = true;
-  buttonPause.hidden = false;
-  var minutes_interval = setInterval(minutesDecres, 60000);
-  var seconds_interval = setInterval(secondsDecres, 1000);
+  buttonStop.hidden = false;
 
-  minutes = minutes_storage;
+  let seconds_interval = setInterval(diminuirSegundos, 1000);
+  console.log(minutes);
+
+  minutes = minutes_storage.toString().padStart(2, "0");
   seconds = seconds_storage;
 
-  function minutesDecres() {
-    minutes = minutes - 1;
-    document.getElementById("minutes").innerHTML = minutes
-      .toString()
-      .padStart(2, "0");
-  }
-
-  function secondsDecres() {
+  function diminuirSegundos() {
     seconds = seconds - 1;
     document.getElementById("seconds").innerHTML = seconds
       .toString()
       .padStart(2, "0");
-
     if (seconds <= 0) {
       if (minutes <= 0) {
-        clearInterval(minutes_interval);
         clearInterval(seconds_interval);
-
-        document.getElementById("done").innerHTML =
-          "Pomodoro completo! Faça um intervalo!";
-        buttonPause.hidden = true;
-        buttonStart.hidden = true;
-        sound.play();
+        document.getElementById("done").innerHTML = "Pomodoro completo.";
+        buttonStop.hidden = true;
+        buttonStart.hidden = false;
       }
-      seconds = 60;
+      seconds = 59;
+      minutes = minutes - 1;
+      document.getElementById("seconds").innerHTML = seconds;
+      document.getElementById(
+        "minutes"
+      ).innerHTML = minutes.toString().padStart(2, "0");
     }
   }
 
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
 
-  buttonPause.addEventListener("click", function () {
+  buttonStop.addEventListener("click", function () {
     minutes_storage = document.getElementById("minutes").innerHTML;
     seconds_storage = document.getElementById("seconds").innerHTML;
-    clearInterval(minutes_interval);
     clearInterval(seconds_interval);
-    buttonPause.hidden = true;
+    buttonStop.hidden = true;
     buttonStart.hidden = false;
   });
 });
